e54467888d338b304fc1fe07c08ac6cfd768edb1
Маша <mbanakova@yandex.ru>
2021-04-22 01:33:36 +0300

    first commit

---
+++ source/js/script.js
@@ -0,0 +1,20 @@
+(function () {
+  // переключает видимость пароля
+  let password = document.querySelector("#password");
+  let eye = document.querySelector("#password-toggle");
+
+  function toggleEye() {
+    if (password.type === "password") {
+      password.setAttribute("type", "text");
+      eye.classList.add("form__eye--visible");
+    } else {
+      password.setAttribute("type", "password");
+      eye.classList.remove("form__eye--visible");
+    }
+  }
+
+  eye.addEventListener("click", (event) => {
+    event.preventDefault();
+    toggleEye();
+  });
+})();
