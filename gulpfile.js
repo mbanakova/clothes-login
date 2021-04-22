ad48c72a99556ac7b74782e48adab759c79c5ee0
Маша <mbanakova@yandex.ru>
2021-04-22 23:43:40 +0300

    final

--- gulpfile.js
+++ gulpfile.js
@@ -8,40 +8,32 @@ var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
-var imageminJpegtran = require('imagemin-jpegtran');
+var imageminJpegtran = require("imagemin-jpegtran");
 var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
 var del = require("del");
-var htmlmin = require('gulp-htmlmin');
-// var uglify = require("gulp-uglify");
-var babel = require('gulp-babel');
-
+var htmlmin = require("gulp-htmlmin");
+var babel = require("gulp-babel");
 
 gulp.task("clean", function () {
   return del("build");
 });
 
-
 gulp.task("copy", function () {
-  return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/*.ico"
-  ], {
-    base: "source"
-  })
+  return gulp
+    .src(["source/fonts/**/*.{woff,woff2}", "source/img/**", "source/*.ico"], {
+      base: "source",
+    })
     .pipe(gulp.dest("build"));
 });
 
-
 gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
+  return gulp
+    .src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([autoprefixer()]))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
@@ -49,52 +41,57 @@ gulp.task("css", function () {
     .pipe(server.stream());
 });
 
-
 gulp.task("images", function () {
-    return gulp.src("source/img/**/*.{jpg,png,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({ optimizationLevel: 3 }),
-      imageminJpegtran({ progressive: true }),
-      imagemin.svgo()
-    ]))
+  return gulp
+    .src("source/img/**/*.{jpg,png,svg}")
+    .pipe(
+      imagemin([
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imageminJpegtran({ progressive: true }),
+        imagemin.svgo(),
+      ])
+    )
     .pipe(gulp.dest("source/img"));
 });
 
-
 gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{jpg,png}")
+  return gulp
+    .src("source/img/**/*.{jpg,png}")
     .pipe(webp({ quality: 90 }))
-    .pipe(gulp.dest("source/img"))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
+  return gulp
+    .src("source/img/icon-*.svg")
+    .pipe(
+      svgstore({
+        inlineSvg: true,
+      })
+    )
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
 });
 
-
 gulp.task("html", function () {
-  return gulp.src("source/*.html")
+  return gulp
+    .src("source/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"));
 });
 
-
 gulp.task("js", function () {
-  return gulp.src("source/js/**/*.js")
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
+  return gulp
+    .src("source/js/**/*.js")
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      })
+    )
     .pipe(sourcemap.init())
-    // .pipe(uglify())
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/js"))
+    .pipe(gulp.dest("build/js"));
 });
-
 
 gulp.task("server", function () {
   server.init({
@@ -102,7 +99,7 @@ gulp.task("server", function () {
     notify: false,
     open: true,
     cors: true,
-    ui: false
+    ui: false,
   });
 
   gulp.watch("source/sass/**/*.scss", gulp.series("css"));
@@ -110,13 +107,15 @@ gulp.task("server", function () {
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
   gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
-})
+});
 
 gulp.task("refresh", function (done) {
   server.reload();
   done();
 });
 
-
-gulp.task("build", gulp.series("clean", "webp", "copy", "css", "sprite", "html", "js"));
+gulp.task(
+  "build",
+  gulp.series("clean", "webp", "copy", "css", "sprite", "html", "js")
+);
 gulp.task("start", gulp.series("build", "server"));
--- source/js/script.js
+++ source/js/script.js
@@ -1,22 +1,26 @@
-(function () {
-  // переключает видимость пароля
-  let password = document.querySelector("#password");
-  let eye = document.querySelector("#password-toggle");
+// переключает видимость пароля
+let password = document.querySelector("#password");
+let eye = document.querySelector("#password-toggle");
 
-  function toggleEye() {
-    if (password.type === "password") {
-      password.setAttribute("type", "text");
-      eye.classList.add("form__eye--visible");
-    } else {
-      password.setAttribute("type", "password");
-      eye.classList.remove("form__eye--visible");
-    }
+function toggleEye() {
+  if (password.type === "password") {
+    password.setAttribute("type", "text");
+    eye.classList.add("form__eye--visible");
+  } else {
+    password.setAttribute("type", "password");
+    eye.classList.remove("form__eye--visible");
   }
+}
 
-  if (eye) {
-    eye.addEventListener("click", (event) => {
-      event.preventDefault();
-      toggleEye();
-    });
-  }
-})();
+if (eye) {
+  eye.addEventListener("click", (event) => {
+    event.preventDefault();
+    toggleEye();
+  });
+}
+
+// маска телефона
+let phone = document.querySelector("#phone");
+
+let im = new Inputmask("+7(999)999-99-99");
+im.mask(phone);
